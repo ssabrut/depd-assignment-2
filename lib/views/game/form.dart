97e6736ff8f601d6a8d8e2e896804f9c6574a092
ssabrut <michael.gunawan2002@gmail.com>
2022-10-11 19:59:36 +0700

    Add download game form

--- lib/views/game/form.dart
+++ lib/views/game/form.dart
@@ -11,9 +11,7 @@ class _PlayFormState extends State<PlayForm> {
   final usernameController = TextEditingController();
   final emailController = TextEditingController();
   final phoneNumController = TextEditingController();
-  final cityController = TextEditingController();
   final _playKey = GlobalKey<FormState>();
-  bool isVisible = true;
 
   @override
   void dispose() {
@@ -21,7 +19,6 @@ class _PlayFormState extends State<PlayForm> {
     usernameController.dispose();
     emailController.dispose();
     phoneNumController.dispose();
-    cityController.dispose();
   }
 
   @override
@@ -53,6 +50,7 @@ class _PlayFormState extends State<PlayForm> {
                         : null;
                   },
                 ),
+                const SizedBox(height: 16.0),
                 TextFormField(
                   keyboardType: TextInputType.emailAddress,
                   decoration: const InputDecoration(
@@ -61,13 +59,84 @@ class _PlayFormState extends State<PlayForm> {
                   autovalidateMode: AutovalidateMode.onUserInteraction,
                   validator: (value) {
                     return !EmailValidator.validate(value.toString())
-                        ? "Email tidak valid"
+                        ? "Email not valid"
                         : null;
                   },
-                )
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  keyboardType: TextInputType.phone,
+                  decoration: const InputDecoration(
+                      labelText: "Phone number", prefixIcon: Icon(Icons.phone)),
+                  controller: phoneNumController,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    return value.toString().length < 12
+                        ? "Phone number not valid"
+                        : null;
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                    onPressed: () {
+                      if (_playKey.currentState!.validate()) {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text("Download confirmation"),
+                                content: Text(
+                                    "Username: ${usernameController.text.toString()}\nEmail: ${emailController.text.toString()}\nPhone number: ${phoneNumController.text.toString()}"),
+                                actions: <Widget>[
+                                  TextButton(
+                                      onPressed: () => Navigator.pop(context),
+                                      child: const Text("OK"))
+                                ],
+                              );
+                            });
+                      } else {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text("Download failed!"),
+                                content:
+                                    const Text("Please fill all form fields!"),
+                                actions: <Widget>[
+                                  TextButton(
+                                      onPressed: () => Navigator.pop(context),
+                                      child: const Text("OK"))
+                                ],
+                              );
+                            });
+                      }
+                    },
+                    child: const Text("Download now")),
               ],
             ),
           ),
         ));
   }
 }
+
+// if (_playKey.currentState!.validate()) {
+    //   AlertDialog(
+    //     title: const Text("Download confirmation"),
+    //     content: const Text("Success"),
+    //     actions: <Widget>[
+    //       TextButton(
+    //           onPressed: () => Navigator.pop(context, "OK"),
+    //           child: const Text("OK"))
+    //     ],
+    //   );
+    // } else {
+    //   AlertDialog(
+    //     title: const Text("Download failed!"),
+    //     content: const Text("Please fill all form fields!"),
+    //     actions: <Widget>[
+    //       TextButton(
+    //           onPressed: () => Navigator.pop(context, "OK"),
+    //           child: const Text("OK"))
+    //     ],
+    //   );
+    // }
